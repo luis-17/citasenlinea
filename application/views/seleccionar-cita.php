f6e82c8a64f9575d2a24fa0042487292b09583f5
Yeraldine Martínez <yerald02@hotmail.com>
2017-04-07 18:08:15 -0500

    Comenzando seleccion de cita

--- application/views/seleccionar-cita.php
+++ application/views/seleccionar-cita.php
@@ -1,8 +1,19 @@
 <div class="content container" ng-controller="programarCitaController">       
       <div class="container-fluid "  style="visibility: visible; opacity: 1; display: block; transform: translateY(0px);">
-          <div class="col-md-12">
+          <div class="col-sm-12 col-md-12">
               <div class="row">
-              	
+	              <div class="col-sm-12 col-md-2">
+	              	<div class="text-guia">
+	              		<p class="saludo">¡Hola! {{fSessionCI.nombres}}</p>
+	              		<p class="instruccion">Programar tu cita es muy sencillo... Escoge la sede, la especialidad y un Médico si tienes alguno favorito</p>
+	              	</div>
+	              	<div class="content-flecha" style="margin-top:110px;">
+	              		<div class="flecha"><div class="i-flecha"><i class="fa fa-arrow-right"></i></div></div>
+	              	</div>
+	              </div>
+	              <div class="col-sm-12 col-md-9">
+	              	
+	              </div>
               </div>
           </div>
       </div>
