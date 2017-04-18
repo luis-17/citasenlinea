ced583b154dea14b76f6e31fbbb6fe19a34e3a6b
Yeraldine Martínez <yerald02@hotmail.com>
2017-04-18 10:39:37 -0500

    dependencia de culqi: requests

--- application/libraries/culqi_php.php
+++ application/libraries/culqi_php.php
@@ -3,6 +3,8 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Culqi_php {
 	public function Culqi_php(){
+		require_once('requests.php');
+		Requests::register_autoloader();
 		require_once('culqi.php');
 	}
 }
