1ae5b79644e4734b42d008de9275defc082cae9a
Luis Ricardo <luis-ls17@hotmail.com>
2018-04-14 17:58:56 -0500

    delete phpmailer

--- application/libraries/my_phpmailer.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class My_PHPMailer {
-	public function __construct()
-	{
-	    require_once('phpmailer/class.phpmailer.php');
-	}
-}
-?>
